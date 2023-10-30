31f0b2b63bb09374eb42f78fc2d5a21a790d6c0c
kirman89 <kirman1989.2@gmail.com>
2023-10-31 00:01:21 +0200

    HT_05 Tasks_4-5

--- HT_05/task_5.py
+++ HT_05/task_5.py
@@ -6,3 +6,33 @@
 Не забудьте протестувати з різними значеннями на предмет помилок!
 """
 
+def calc(expresions):
+    operations = ['+', '-', '*', '/', '%', '//', '**']
+    exp_list = expresions.split(' ')
+
+    try:
+        if exp_list[1] == '+':
+            return float(exp_list[0]) + float(exp_list[2])
+        elif exp_list[1] == '-':
+            return float(exp_list[0]) - float(exp_list[2])
+        elif exp_list[1] == '*':
+            return float(exp_list[0]) * float(exp_list[2])
+        elif exp_list[1] == '/':
+            return float(exp_list[0]) / float(exp_list[2])
+        elif exp_list[1] == '%':
+            return float(exp_list[0]) % float(exp_list[2])
+        elif exp_list[1] == '//':
+            return float(exp_list[0]) // float(exp_list[2])
+        elif exp_list[1] == '**':
+            return float(exp_list[0]) ** float(exp_list[2])
+
+    except ZeroDivisionError:
+        print("Zero division error! Change value or operator")
+        return False
+
+    except ValueError:
+        print("Incorrect input! Check your value and operator")
+        return False
+
+expr_input = input("Enter expresion in format 'arg1 operator arg2':\n")
+print(calc(expr_input))
