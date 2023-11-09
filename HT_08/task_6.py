b4e926cb0533f9236f231dfdfc9d023d8ad8b2b2
kirman89 <kirman1989.2@gmail.com>
2023-11-09 09:27:05 +0200

    HT_08 task_6 Update

--- HT_08/task_6.py
+++ HT_08/task_6.py
@@ -3,8 +3,26 @@
 """
 
 
+#def shortest_word_length(input_string):
+#    return min([len(word) for word in input_string.split()])
+
+
 def shortest_word_length(input_string):
-    return min([len(word) for word in input_string.split()])
+    words_list = input_string.split()
+
+    def get_next_word(words_list):
+        for word in words_list:
+            yield word
+
+    words = get_next_word(words_list)
+    
+    shortest = len(next(words))
+
+    while True:
+        try:
+            shortest = min(len(next(words)), shortest)
+        except:
+            return shortest
 
 
 print(shortest_word_length('shortest word length'))
