f6b6fe0284593e17558d1bf4aea1c2bd3fc2a5cb
kirman89 <kirman1989.2@gmail.com>
2023-11-09 08:57:40 +0200

    HT_08 task_7 Update

--- HT_08/task_7.py
+++ HT_08/task_7.py
@@ -9,8 +9,25 @@
 """
 
 
+#def array_diff(list_1, list_2):
+#    return [elem for elem in list_1 if elem not in list_2]
+
+
 def array_diff(list_1, list_2):
-    return [elem for elem in list_1 if elem not in list_2]
+
+    def get_different_element(list_1, list_2):
+        for elem in list_1:
+            if elem not in list_2:
+                yield elem
+
+    result_list = []
+    difference = get_different_element(list_1, list_2)
+
+    while True:
+        try:
+            result_list.append(next(difference))
+        except:
+            return result_list
 
 
 print(array_diff([1, 2, 2, 2, 4, 3, 4], [2]))
